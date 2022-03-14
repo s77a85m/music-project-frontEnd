acd3fef5de2daf69bca785a953c25d887b89186c
s77a85m <alimusavi77@gmail.com>
2022-03-14 08:20:51 +0330

    direction and dark mode is complated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ["./src/**/*.html"],
+  darkMode: 'class',
   theme: {
+    backgroundPosition: {
+      'left-center': 'left 0.5rem center'
+    },
     container: {
       center: true,
       padding: '1rem',
     },
     fontFamily:{
-      'body': 'IRANSans'
+      'iransanse': 'IRANSansX'
     },
     extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-8deg)' },
+          '50%': { transform: 'rotate(8deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
       padding:{
         'md':'56%'
       },
+      colors:{
+        dark:{
+          800: '#232326',
+          700: '#323236',
+          600: '#3b3b41',
+        }
+      }
     },
   },
   plugins: [
